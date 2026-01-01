fa62654c32526e729b6ea8121b16fee258d00750
raghuveerbhat <raghuveer.9bhat@gmail.com>
2026-01-01 23:12:33 +0000

    Implement unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,56 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from ml.data import process_data
+from ml.model import train_model, inference
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+# Just creating a synthetic dataset to be used for testing
+@pytest.fixture
+def data():
+    return pd.DataFrame({
+        "workclass": ["Private", "Private", "State-gov"],
+        "education": ["Bachelors", "Masters", "HS-grad"],
+        "marital-status": ["Married", "Single", "Divorced"],
+        "occupation": ["Exec", "Prof", "Repair"],
+        "relationship": ["Husband", "Wife", "Own-child"],
+        "race": ["White", "Black", "Asian"],
+        "sex": ["Male", "Female", "Male"],
+        "native-country": ["US", "US", "Mexico"],
+        "salary": [">50K", "<=50K", "<=50K"]
+    })
+
+def test_process_data_count(data):
+    """ Test that processing does not lose any rows of data. """
+    X, y, encoder, lb = process_data(
+        data, 
+        categorical_features=["workclass", "education", "marital-status", "occupation", 
+                              "relationship", "race", "sex", "native-country"], 
+        label="salary", 
+        training=True
+    )
+    
+    # Check that we still have 3 rows
+    assert len(X) == 3
+    assert len(y) == 3
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+def test_train_model_exists(data):
+    """ Test that the training function returns a model object. """
+    X, y, _, _ = process_data(data, categorical_features=["workclass", "education", "marital-status", "occupation", 
+                              "relationship", "race", "sex", "native-country"], label="salary", training=True)
+    model = train_model(X, y)
+    
+    # Check that model is not None
+    assert model is not None
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+def test_inference_binary(data):
+    """ Test that inferences result in 0 or 1, not some other number. """
+    X, y, _, _ = process_data(data,categorical_features=["workclass", "education", "marital-status", "occupation", 
+                              "relationship", "race", "sex", "native-country"], label="salary", training=True)
+    model = train_model(X, y)
+    preds = inference(model, X)
+    
+    # Check that every prediction is either 0 or 1
+    assert np.all((preds == 0) | (preds == 1))
