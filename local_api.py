3f6c78d10d2836eeb951070c415b5901eccf2a31
raghuveerbhat <raghuveer.9bhat@gmail.com>
2026-01-11 22:50:32 +0000

    Update local_api.py and main.py to implement GET and POST requests, and add a screenshot of the local API

--- local_api.py
+++ local_api.py
@@ -2,13 +2,10 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000/")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+print(f"GET:: Status Code: {r.status_code}")
+print(f"GET:: Result: {r.json()['message']}")
 
 
 
@@ -29,10 +26,7 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print(f"POST:: Status Code: {r.status_code}")
+print(f"POST:: Result: {r.json()['result']}")
